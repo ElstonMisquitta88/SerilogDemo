79203381f6a818ec24355bd0a6b5d88e545701d6
Elston Misquitta <81861634+ElstonMisquitta88@users.noreply.github.com>
2022-07-10 11:31:09 +0530

    Changes

---
+++ SerilogDemoApp/SerilogDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerilogDemo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("You Requested the Index Page");
+
+            try
+            {
+                for (int i = 0; i <= 10; i++)
+                {
+                    if (i == 5)
+                    {
+                        throw new Exception("Demo Exception");
+                    }
+                    else
+                    {
+                        _logger.LogInformation("Value of i {LoopCounterValue}", i);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, " Custom Exception Caught");
+            }
+        }
+    }
+}
---
+++ SerilogDemoApp/SerilogDemo/Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerilogDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            // Configuring the Serilog settings via the appsetting.json file
+
+            // STEP 1
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // STEP 2 -- Manage Nuget Packages
+            // (a) Serilog.aspnetcore
+            // (b) Serilog.settings.configuration
+            // (c) Serilog.Enrichers.Environment
+            // (d) Serilog.Enrichers.Thread
+            // (e) Serilog.Enrichers.Process
+
+            // STEP 3  -- (SeriLog)
+            Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+
+            try
+            {
+                Log.Information("Application Starting Up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex.ToString(), "App Failed to Start Correctly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog() // STEP 4
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
